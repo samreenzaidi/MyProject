bacfdb35dee7824b9f88cee1595bdf6d76ebc871
samreenzaidi <36235936+samreenzaidi@users.noreply.github.com>
2019-08-23 14:03:16 +0530

    Delete mainController.js

--- controller/mainController.js
+++
@@ -1,5 +0,0 @@
-app.controller("mainController", ["$scope","$window","$rootScope", function ($scope,$window,$rootScope) {  
-    $scope.email = $rootScope.email;
-    $scope.password = $rootScope.password;
-    $scope.domain = $rootScope.domain;
-}]);
